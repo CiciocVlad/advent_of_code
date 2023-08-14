d2adaa86274870849ce7f7b4c33260707c8d9f8d
Cicioc Vlad <ciciocvlad@gmail.com>
2023-08-14 15:02:05 +0300

    replaced tabs with spaces

--- 2022/day1/solve.clj
+++ 2022/day1/solve.clj
@@ -2,35 +2,35 @@
 
 (defn split [arg]
   (loop [res (split-with (complement #{""}) arg)
-		 result []]
-	(if (= (first res) ())
-	  result
-	  (recur (split-with (complement #{""}) (rest (first (rest res)))) (conj result (first res))))))
+         result []]
+    (if (= (first res) ())
+      result
+      (recur (split-with (complement #{""}) (rest (first (rest res)))) (conj result (first res))))))
 
 (defn turn_to_int [arg]
   (cond
-	(empty? arg) nil
-	(= (first arg) "") (recur (rest arg))
-	:else (cons (Integer/parseInt (first arg)) (turn_to_int (rest arg)))))
+    (empty? arg) nil
+    (= (first arg) "") (recur (rest arg))
+    :else (cons (Integer/parseInt (first arg)) (turn_to_int (rest arg)))))
 
 (defn get_arr [string]
   (def lines (str/split string #"\n"))
   (->>
-	lines
-	(split)
-	(map turn_to_int)
-	(map #(reduce + %))))
+    lines
+    (split)
+    (map turn_to_int)
+    (map #(reduce + %))))
 
 (defn get_max [string]
   (apply max (get_arr string)))
 
 (defn get_top_3 [string]
   (->>
-	string
-	(get_arr)
-	(sort)
-	(reverse)
-	(take 3)))
+    string
+    (get_arr)
+    (sort)
+    (reverse)
+    (take 3)))
 
 (defn get_result [string]
   (def part1 (get_max string))
@@ -39,6 +39,6 @@
 
 (defn main []
   (let [input (read-line)]
-	(println (get_result (slurp input)))))
+    (println (get_result (slurp input)))))
 
 (main)
